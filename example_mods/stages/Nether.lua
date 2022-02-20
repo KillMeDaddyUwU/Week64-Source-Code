6763c9f06c6b8918c2d8d70ebf594b1b98f59991
KillMeDaddyUwU <96146945+KillMeDaddyUwU@users.noreply.github.com>
2022-02-20 17:42:32 -0300

    first commit

---
+++ example_mods/stages/Nether.lua
@@ -0,0 +1,8 @@
+function onCreate()
+    makeLuaSprite('Background','Background',0,0)
+    addLuaSprite('Background')
+    makeLuaSprite('Main Ground','Main Ground',0,0)
+    addLuaSprite('Main Ground')
+
+    close(true)
+end
